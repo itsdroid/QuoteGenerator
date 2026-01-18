da108c41652f625475f7760a06e8682ad30305e9
itsdroid <shubhampopalghat77@gmail.com>
2026-01-18 23:56:12 +0530

    updated api

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-const quotes = [
-            "Be yourself; everyone else is already taken.",
-            "So many books, so little time.",
-            "A room without books is like a body without a soul.",
-            "You only live once, but if you do it right, once is enough.",
-            "Be the change that you wish to see in the world."
-        ];
-
-        function generate() {
-            const index = Math.floor(Math.random() * quotes.length);
-            document.getElementById("quote-text").innerText = quotes[index];
-        }
+async function generate() {
+    const response = await fetch("https://dummyjson.com/quotes/random");
+    const data = await response.json();
+    document.getElementById("quote-text").innerText = data.quote;
+}
